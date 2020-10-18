732453f3587b05c5c6ce6846f07c846702c7c8be
Francisca Hohlberg <fhohlberg@uc.cl>
2020-10-18 17:44:36 -0300

    cambios presentacion

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module G24Proyecto
+module G24Proyecto-Mapen
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
